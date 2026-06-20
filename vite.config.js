ae3f16327d33ea1cd2f9f69f2b6332a1b69c94e7
avz-sharma <115913111+avz-sharma@users.noreply.github.com>
2026-06-20 16:08:57 +0530

    Change base path in vite.config.js to root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+  base: "/",
   envPrefix: ['VITE_', 'GEMINI_'],
 });
